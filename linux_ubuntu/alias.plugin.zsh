bd7a83676df94c5202529f88a1d328d7cf75bcb6
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-05-23 17:07:15 +0530

    Till >>> 23-May-2023, 17:07:14
    MAKE CHANGES IN [alias.plugin.zsh]

--- linux_ubuntu/alias.plugin.zsh
+++ linux_ubuntu/alias.plugin.zsh
@@ -7,11 +7,13 @@ alias ncf="nvim ~/.config/nvim/init.vim"
 alias gdir='cd ~/Desktop/programing/github/'
 alias pydir='cd ~/Desktop/programing/python/practic'
 alias cdir='cd ~/Desktop/programing/c_lan'
+alias spdir='cd ~/Desktop/programing/system_script/'
 
 alias exiftool_f='exiftool -overwrite_original'
 alias eza='nvim ~/.oh-my-zsh/plugins/alias/alias.plugin.zsh'
 alias pushcon='python3 ~/Desktop/programing/system_script/Push_Config_To_Github.py'
 alias rename_='python3 ~/Desktop/programing/system_script/Rename.py'
 alias ff_pp='python3 ~/Desktop/programing/system_script/Play_Vdo.py'
+alias f_p='python3 ~/Desktop/programing/system_script/Play_ffp.py'
 alias ff_r='ffmpeg -f x11grab -s 1366x768 -i :0.0 -f alsa -i hw:0 "/home/pritam/Videos/screencasts/ffmeg_$(date +'%Y-%m-%d_%H-%M-%S').mkv"' # Record screen by using 'ffmpeg'
 alias scrcpy_r='scrcpy -m 1080 --max-fps=60 -r ~/Videos/screencasts/scrcpy_$(date +'%Y-%m-%d_%H-%M-%S').mp4'
