b4fbf00c3e226b5be6b4b1fd68d012c84f1a0d08
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-05-24 18:54:01 +0530

    Till >>> 24-May-2023, 18:53:55
    MAKE CHANGES IN [rc.conf, alias.plugin.zsh]

--- linux_ubuntu/alias.plugin.zsh
+++ linux_ubuntu/alias.plugin.zsh
@@ -3,6 +3,7 @@
 alias lz='lazygit'
 alias erc='nvim ~/.zshrc'
 alias ncf="nvim ~/.config/nvim/init.vim" 
+alias addgitignore="python3 ~/Desktop/programing/system_script/git_lgnore/Mk_Git_Ignore.py"
 
 alias gdir='cd ~/Desktop/programing/github/'
 alias pydir='cd ~/Desktop/programing/python/practic'
@@ -14,6 +15,6 @@ alias eza='nvim ~/.oh-my-zsh/plugins/alias/alias.plugin.zsh'
 alias pushcon='python3 ~/Desktop/programing/system_script/Push_Config_To_Github.py'
 alias rename_='python3 ~/Desktop/programing/system_script/Rename.py'
 alias ff_pp='python3 ~/Desktop/programing/system_script/Play_Vdo.py'
-alias f_p='python3 ~/Desktop/programing/system_script/Play_ffp.py'
+alias f_p='python3 ~/Desktop/programing/system_script/Play_Ffp.py'
 alias ff_r='ffmpeg -f x11grab -s 1366x768 -i :0.0 -f alsa -i hw:0 "/home/pritam/Videos/screencasts/ffmeg_$(date +'%Y-%m-%d_%H-%M-%S').mkv"' # Record screen by using 'ffmpeg'
 alias scrcpy_r='scrcpy -m 1080 --max-fps=60 -r ~/Videos/screencasts/scrcpy_$(date +'%Y-%m-%d_%H-%M-%S').mp4'
